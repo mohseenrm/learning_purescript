f9d1369c77c7b2bf6a197bdad9dd5b47753ef2b1
mohseenrm <mohseenmukaddam6@gmail.com>
2016-11-26 11:49:43 -0700

    adding resource links

--- test/Main.purs
+++ test/Main.purs
@@ -5,5 +5,8 @@ import Prelude
 import Euler (answer)
 import Test.Assert (assert)
 
+-- https://leanpub.com/purescript/read
+-- https://github.com/purescript/purescript/wiki/Language-Guide#comments
+
 main = do
   assert (answer == 233168)

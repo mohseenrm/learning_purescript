ccda380f1d13e1d2701a9b3c1d9a1036d39c577b
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-03 22:07:17 -0700

    some progress

--- src/Euler2.purs
+++ src/Euler2.purs
@@ -8,6 +8,12 @@ import Euler (answer)
 --individual values should not exceed 40000000
 --sum of even valued numbers
 
+find_limit :: Int -> Int -> Boolean
+find_limit x big_number = if fib x < big_number then true else false
+
+
+
+
 fib :: Int -> Int
 fib 0 = 1
 fib 1 = 2
